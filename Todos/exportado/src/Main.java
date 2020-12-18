52b0b9d3fdf46e281da569852a49b8842cd4bb7b
PMUbide <75802506+PMUbide@users.noreply.github.com>
2020-12-18 16:47:23 +0100

    otra subida

--- Todos/exportado/src/Main.java
+++ Todos/exportado/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
 		Scanner entrada = new Scanner(System.in);
-		System.out.print("Introduce un entero: ");
+		System.out.print("Introduce un número entero que quieras:  ");
 		int num = entrada.nextInt();
 		Funciones.bucle(num);
 		
