48ef74a5701aa617988fefc494f22116dd908357
PMUbide <75802506+PMUbide@users.noreply.github.com>
2021-04-22 13:09:00 +0200

    "Ejemplo2 again"

--- Todos/Recursividad/src/Ejemplo2.java
+++ Todos/Recursividad/src/Ejemplo2.java
@@ -6,6 +6,7 @@ public class Ejemplo2 {
 		//Anda que aquí practicariamos la increible recursividad.!
 		//Si señor! ME parece que de momento va. Ahora haremos otro commit desde Eclipse hasta
 		//El otro pc a ver si va bien el eclipse después.!
+		//Vale pues ahora otra vez lo de antes! volver a subir esta y ver en el portatil si carga bien eclipse!
 	}
 
 }
