f7d5a13353512c658c51dc021f00c930bb3fc57b
PMUbide <pablomartinez92@hotmail.com>
2021-04-22 13:06:55 +0200

    "Subido el ejemplo2"

--- Todos/Recursividad/src/Ejemplo2.java
+++ Todos/Recursividad/src/Ejemplo2.java
@@ -4,6 +4,8 @@ public class Ejemplo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Anda que aquí practicariamos la increible recursividad.!
+		//Si señor! ME parece que de momento va. Ahora haremos otro commit desde Eclipse hasta
+		//El otro pc a ver si va bien el eclipse después.!
 	}
 
 }
