3ed18d0ac047aa90f647b7d10a40e7de4384aa5e
PMUbide <pablomartinez92@hotmail.com>
2021-04-22 19:38:27 +0200

    jueves

--- Todos/Recursividad/src/Ejemplo2.java
+++ Todos/Recursividad/src/Ejemplo2.java
@@ -3,10 +3,15 @@ public class Ejemplo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Anda que aquí practicariamos la increible recursividad.!
-		//Si señor! ME parece que de momento va. Ahora haremos otro commit desde Eclipse hasta
-		//El otro pc a ver si va bien el eclipse después.!
-		//Vale pues ahora otra vez lo de antes! volver a subir esta y ver en el portatil si carga bien eclipse!
+		
+//		int prob_difficulty = (int) (Math.random() * 100);
+		mostrarDifi();
+		
 	}
 
+	public static void mostrarDifi() {
+		System.out.println((int) (Math.random() * 100));
+		mostrarDifi();
+	}
+	
 }
