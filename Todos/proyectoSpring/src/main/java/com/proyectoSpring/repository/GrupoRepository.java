34e5fb5c7294c186736ce237e43932f26eaa3228
PMUbide <75802506+PMUbide@users.noreply.github.com>
2021-04-22 13:02:23 +0200

    Puesto en local y prueba de commit desde eclipse

--- Todos/proyectoSpring/src/main/java/com/proyectoSpring/repository/GrupoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyectoSpring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.proyectoSpring.model.Grupo;
-
-
-public interface GrupoRepository extends CrudRepository<Grupo, Long>{
-
-}
--- Todos/proyectoSpring/src/main/java/com/proyectoSpring/repository/RutaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.proyectoSpring.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.proyectoSpring.model.Ruta;
-
-public interface RutaRepository extends CrudRepository<Ruta, Long>{
-
-}
